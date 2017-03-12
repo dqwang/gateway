c5f10ab900a3a7a111dde76ae84fa88d7a87b9ac
dqwang <dequan.q.wang@nsn.com>
2017-03-12 20:19:30 +0800

    beep ok

---
+++ Objects/main.d
@@ -0,0 +1,15 @@
+.\objects\main.o: USER CODE\main.c
+.\objects\main.o: .\include\main.h
+.\objects\main.o: .\LPC8xx\LPC8xx.h
+.\objects\main.o: .\CM0+\core_cm0plus.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: .\CM0+\core_cmInstr.h
+.\objects\main.o: .\CM0+\core_cmFunc.h
+.\objects\main.o: .\LPC8xx\system_LPC8xx.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\main.o: .\LPC8xx\uart.h
+.\objects\main.o: .\include\main.h
+.\objects\main.o: .\include\type.h
+.\objects\main.o: .\include\gpio.h
---
+++ Objects/uart.d
@@ -0,0 +1,15 @@
+.\objects\uart.o: LPC8xx\uart.c
+.\objects\uart.o: .\include\main.h
+.\objects\uart.o: .\LPC8xx\LPC8xx.h
+.\objects\uart.o: .\CM0+\core_cm0plus.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart.o: .\CM0+\core_cmInstr.h
+.\objects\uart.o: .\CM0+\core_cmFunc.h
+.\objects\uart.o: .\LPC8xx\system_LPC8xx.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\uart.o: .\LPC8xx\uart.h
+.\objects\uart.o: .\include\main.h
+.\objects\uart.o: .\include\type.h
+.\objects\uart.o: .\include\gpio.h
